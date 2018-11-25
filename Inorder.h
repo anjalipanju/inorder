dd09eb06f3aa4d41929f1550209de831a0ff7261
anjalipanju <45040570+anjalipanju@users.noreply.github.com>
2018-11-25 11:12:42 +0530

    Create Inorder.h

---
+++ Inorder.h
@@ -0,0 +1,25 @@
+#include<stdio.h> 
+struct tNode 
+{ 
+
+   int data; 
+
+   struct tNode* left; 
+
+   struct tNode* right; 
+}; 
+struct sNode 
+{ 
+
+  struct tNode *t; 
+
+  struct sNode *next; 
+}; 
+
+/* Stack related functions */
+
+void push(struct sNode** top_ref, struct tNode *t); 
+
+struct tNode *pop(struct sNode** top_ref); 
+
+bool isEmpty(struct sNode *top); 
